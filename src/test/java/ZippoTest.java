7e65f5fe4a3f02f8c8223b8ea4b7f341e46bbbef
daulet2030 <daulet2030@gmail.com>
2020-08-28 20:50:55 +0600

    restassured

---
+++ src/test/java/ZippoTest.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class ZippoTest {
+    @Test
+    public void getTest() {
+        given().
+                when().
+                then();
+    }
+}
