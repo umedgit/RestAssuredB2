cc5b3f57c0fb0de7d74345048b68fd351f06e92b
daulet2030 <daulet2030@gmail.com>
2020-08-31 22:25:23 +0600

    goRest creating a user

--- src/test/java/goRest/GoRestTests.java
+++ src/test/java/goRest/GoRestTests.java
@@ -1,6 +1,8 @@
 package goRest;
 
 import goRest.pojo.User;
+import io.restassured.http.ContentType;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -22,6 +24,7 @@ public class GoRestTests {
             System.out.println(user);
         }
     }
+
     @Test
     public void extractingListOfUsersAsArray() {
         User[] userList = given()
@@ -45,4 +48,27 @@ public class GoRestTests {
 
         System.out.println(user);
     }
+
+    @Test
+    public void creatingUser() {
+        given()
+                // specify Authorization header, body, Content-Type header
+                .header("Authorization", "Bearer 55b19d86844d95532f80c9a2103e1a3af0aea11b96817e6a1861b0d6532eef47")
+                .contentType(ContentType.JSON)
+                .body("{\"email\":\""+randomestring()+"@gmail.com"+"\", \"name\": \"Techno\", \"gender\":\"Male\", \"status\": \"Active\"}")
+                .when()
+                .post("https://gorest.co.in/public-api/users")
+                .then()
+                .statusCode(200)
+        .body("code", equalTo(201))
+        ;
+
+    }
+
+    private String randomestring()
+    {
+        String generatedstring= RandomStringUtils.randomAlphabetic(8);
+        return(generatedstring);
+    }
+
 }
