2361ccc877be3a5d3adeed0ec58b4462ec4be97c
daulet2030 <daulet2030@gmail.com>
2020-09-01 23:35:57 +0600

    basqar creating country

--- src/test/java/basqar/CountryTest.java
+++ src/test/java/basqar/CountryTest.java
@@ -1,7 +1,9 @@
 package basqar;
 
+import basqar.model.Country;
 import io.restassured.http.ContentType;
 import io.restassured.http.Cookies;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -14,6 +16,8 @@ import static org.hamcrest.Matchers.*;
 public class CountryTest {
 
     private Cookies cookies;
+    private String id;
+    private String randomName;
 
     @BeforeClass
     public void init() {
@@ -33,5 +37,26 @@ public class CountryTest {
                 .then()
                 .statusCode(200)
                 .extract().response().detailedCookies();
+    }
+
+    @Test
+    public void createTest() {
+        Country body = new Country();
+        randomName = RandomStringUtils.randomAlphabetic(8);
+        body.setName(randomName);
+        body.setCode(RandomStringUtils.randomAlphabetic(4));
+
+        id = given()
+                .cookies(cookies)
+                .body(body)
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/school-service/api/countries")
+                .then()
+                .statusCode(201)
+                .extract().jsonPath().getString("id");
+
+        System.out.println(id);
+        System.out.println(randomName);
     }
 }
